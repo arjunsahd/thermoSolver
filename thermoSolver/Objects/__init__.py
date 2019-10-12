116c4495003da3140f5664ec43907c997933e7ec
Kaneki99 <arjunsahdev13@gmail.com>
2019-10-12 17:14:18 +0300

    Second

---
+++ thermoSolver/Objects/__init__.py
@@ -0,0 +1,1 @@
+from thermoSolver.Objects import element
---
+++ thermoSolver/Objects/element.py
@@ -0,0 +1,7 @@
+compound = {'Water':           {'SpecificHeatCp': {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'range': [0, 100]},
+                                'BoilingPoint': 100,
+                                'MeltingPoint': 0},
+            'CarbonDioxide':   {'SpecificHeatCp': {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'range': [0, 100]},
+                                'BoilingPoint': 100,
+                                'MeltingPoint': 0}
+            }
